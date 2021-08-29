6317ac828e73e231702b0fc4269c51c62774d248
Mazy-Shame <boss.masyanov@gmail.com>
2021-08-29 16:33:01 +0400

    start

---
+++ src/components/MyComponents.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="root">
+        {{text}}
+        <input type="text" @input="checkinput" >
+        <input type="text">
+        {{ mail }}
+        <div>asdasdads</div>
+        <button
+        v-for="user in users"
+        :key="user"
+        >{{user}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return{
+            text: 'hello world',
+            password: '123',
+            mail: "default",
+            pages: 12,
+            users: ["firstuser","secoduser"]
+        }
+    },
+    
+    methods: {
+        checkinput(event) {
+            this.mail = event.target.value
+        }
+    }
+}
+</script>
